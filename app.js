9b5d59ce30710a2e2ff5797d7ed037267e960d1d
Harpreet Singh <harpreet95hs@gmail.com>
2023-05-15 19:29:12 -0400

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- app.js
+++ app.js
@@ -7,4 +7,16 @@ const $h1 = $('<h1>').text('Hogwarts');
 console.log($h1);
 
 $container.append($h1);
+
+const $h2 = $('<h2>').text('Harp Singh')
+$container.append($h2);
+
+const $h3 = $('<h3>').text('Slytherin')
+$container.append($h3);
+
+const $h4Pet = $('<h4>').text('Buckbeak').addClass('Hippogriff')
+$container.append($h4Pet);
+
+const $h4Wand = $('<h4>').text('Ebony Wand with Unicorn Hair, Slightly Yielding.').addClass('Hippogriff')
+$container.append($h4Wand);
 });
