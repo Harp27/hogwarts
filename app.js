e105a331c6a340646553a458c1a46ef793acbdeb
Harpreet Singh <harpreet95hs@gmail.com>
2023-05-15 19:05:29 -0400

    Commit 1 - I added my first element and it was fun

---
+++ app.js
@@ -0,0 +1,10 @@
+$(() => {
+    // put jQuery in here
+    const $container = $('#container');
+console.log($container);
+
+const $h1 = $('<h1>').text('Hogwarts');
+console.log($h1);
+
+$container.append($h1);
+});
